fca7c664d6b3e09ca5d999cbfffae3693bdd8f02
ragbn <raaghu@outlook.in>
2021-10-27 16:41:51 +1100

    check for prereqs - plugins

--- BackgroundScript.js
+++ BackgroundScript.js
@@ -8,61 +8,32 @@
     var response = request.execute();
 
     var xmlString = response.getBody();
-    //var xmldoc = new global.XMLDocument(xmlString, true);
+
     var xmldoc = new global.XMLDocument2();
     xmldoc.parseXML(xmlString)
     var instance_version = xmldoc.getNodeText("//glide.build.name");
-
-    //var xnList = xmldoc.getNode("//plugin");
-
+    gs.info('Your ServiceNow version is ' + instance_version);
+    gs.info('-------------------------------------------------------');
+    gs.info('Checking for Microsoft Teams Integration Prerequisites');
+    gs.info('-------------------------------------------------------');
     var node = xmldoc.getFirstNode( '/xmlstats/plugin' );
-    //gs.info(node)
+
     j=1;
     while( node != null ) {
-       // var value = xmldoc.getNodeText( '//plugin[' + j + ']');
+
         var value = node.getTextContent(node)
-        //if(value == 'com.glide.cs.chatbot.lite')
-        // gs.info('Virtual Agent lite is Available')
-        gs.info( 'Value: ' + j + value);
+        if(value == 'com.glide.cs.chatbot.lite')
+         gs.info('Virtual Agent lite is Available')
+        if(value == 'com.glide.cs.chatbot')
+         gs.info('Virtual Agent is Available')
+        if(value == 'com.snc.itsm.virtualagent.lite')
+         gs.info('ITSM Virtual Agent Conversation Topics Lite are Available')
+        if( value == 'com.sn_hr_virtual_agent')
+         gs.info( 'HR Service Delivery Virtual Agent Topics are Available');
+        if( value == 'com.snc.notify')
+         gs.info( 'Notify connector is Available');
         j++;
         node = xmldoc.getNextNode(node);
     }
-    gs.info(instance_version);
-//var iter = xnList.getChildNodeIterator();
-//gs.info(iter.hasNext())
-
-/*while(iter.hasNext()){
-
-var n = iter.next();
     
-  gs.info('Node name: ' +   n.getNodeName());
-  
-  //Outputs all child node values of each HOST
-  
-  gs.info('Node Text Content (values): ' + n.getTextContent());
-  
-  
-  gs.info('Node value (xml string): ' + n);
-  
-}*/
-  
-  
-//gs.info(xnList);
 
-
-
-
-try{
-    var rm = new sn_ws.RESTMessageV2();
-    var endpoint = gs.getProperty('glide.servlet.uri') + "xmlstats.do";
-    rm.setEndpoint(endpoint);
-    rm.setHttpMethod('GET');
-    rm.setRequestHeader('content-type', 'application/json');
-    var response = rm.execute();
-    var responseObj = new XMLHelper(response.getBody()).toObject();
-    gs.print("response=" + response.getBody());
-    gs.print("errorMsg=" + response.getErrorMessage());
-    }
-    catch(err){
-    gs.print("err=" + err);
-    }
