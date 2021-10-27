23381de8c49d329e250f9caf586df4ecd5b9341b
ragbn <raaghu@outlook.in>
2021-10-27 14:30:43 +1100

    fixed null plugin return

--- BackgroundScript.js
+++ BackgroundScript.js
@@ -1,30 +1,33 @@
-var request = new sn_ws.RESTMessageV2();
-request.setEndpoint(gs.getProperty('glide.servlet.uri') + "xmlstats.do");
+    var request = new sn_ws.RESTMessageV2();
+    request.setEndpoint(gs.getProperty('glide.servlet.uri') + "xmlstats.do");
 
-request.setHttpMethod('GET');
+    request.setHttpMethod('GET');
 
-request.setRequestHeader("Accept","application/xml");
+    request.setRequestHeader("Accept","application/xml");
 
-var response = request.execute();
+    var response = request.execute();
 
-var xmlString = response.getBody();
+    var xmlString = response.getBody();
+    //var xmldoc = new global.XMLDocument(xmlString, true);
+    var xmldoc = new global.XMLDocument2();
+    xmldoc.parseXML(xmlString)
+    var instance_version = xmldoc.getNodeText("//glide.build.name");
 
-var xmldoc = new global.XMLDocument(xmlString, true);
+    //var xnList = xmldoc.getNode("//plugin");
 
-var instance_version = xmldoc.getNodeText("//glide.build.name");
-
-//var xnList = xmldoc.getNode("//plugin");
-
-var node = xmldoc.getFirstNode( '/xmlstats/plugin' );
-gs.info(node);
-j=1;
-while( node != null ) {
-      var value = xmldoc.getNodeText( '/xmlstats/plugin[' + j + ']');
-       gs.info( 'Value: ' + value);
-       j++;
-       node = xmldoc.getNextNode(node);
-}
-
+    var node = xmldoc.getFirstNode( '/xmlstats/plugin' );
+    //gs.info(node)
+    j=1;
+    while( node != null ) {
+       // var value = xmldoc.getNodeText( '//plugin[' + j + ']');
+        var value = node.getTextContent(node)
+        //if(value == 'com.glide.cs.chatbot.lite')
+        // gs.info('Virtual Agent lite is Available')
+        gs.info( 'Value: ' + j + value);
+        j++;
+        node = xmldoc.getNextNode(node);
+    }
+    gs.info(instance_version);
 //var iter = xnList.getChildNodeIterator();
 //gs.info(iter.hasNext())
 
@@ -46,7 +49,7 @@ var n = iter.next();
   
 //gs.info(xnList);
 
-gs.info(instance_version);
+
 
 
 try{
